43e0153f3b20c61d8392337b05736eafb0c0fd92
Irfan Dayan <engr.irfandayan@gmail.com>
2023-01-17 15:41:49 +0500

    Add hero section

---
+++ components/Hero.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
+type Props = {};
+
+export default function Hero({}: Props) {
+  const [text, count] = useTypewriter({
+    words: ["Hi, I'm Irfan Dayan", "Full Stack Engineer", "<ILoveToCodeMore/>"],
+    loop: true,
+    delaySpeed: 2000,
+  });
+
+  return (
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      <img
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+        src="/assets/irfandayan.jpg"
+        alt=""
+      />
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[8px]">
+          Full Stack Engineer
+        </h2>
+        <h1 className="text-2xl lg:text-3xl font-semibold scroll-px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h1>
+
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
